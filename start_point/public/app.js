d68ec7e547355cdd210bb139d561a6983243b5e1
Katrina Coutts <katcoutts@hotmail.co.uk>
2016-09-26 21:04:43 +0100

    a bit of css and default for width added

--- start_point/public/app.js
+++ start_point/public/app.js
@@ -3,7 +3,7 @@ var addCat = function(name, favouriteFood, image, width){
   var catUL = createCatUL();
   var liName = createLiName(name);
   var liFavouriteFood = createLiFavouriteFood(favouriteFood);
-  var catImage = createCatImage(width, image)
+  var catImage = createCatImage(image, width)
   var cats = document.querySelector('#cats')
   appendCat(cats, catUL, liName, liFavouriteFood, catImage);
 }
@@ -26,9 +26,14 @@ var createLiFavouriteFood = function(favouriteFood){
   return liFavouriteFood;
 }
 
-var createCatImage = function(width, image){
+var createCatImage = function(image, width){
   var catImage = document.createElement('img');
-  catImage.width = width;
+  if (width === undefined){
+    catImage.width = "500";
+  }
+  else {
+    catImage.width = width;
+  }
   catImage.src = image;
   return catImage;
 }
@@ -45,7 +50,8 @@ var app = function(){
   addCat("Boba", "Sock Fluff", "http://66.media.tumblr.com/d1f01bbe0150fda0c40d2151c5eaeac8/tumblr_odlqqskjj61v9cejwo1_400.jpg", "500");
   addCat("Barnaby", "Tuna", "http://65.media.tumblr.com/8a827e13ebb5db7d16e2b1c4cbe7ce70/tumblr_odtp4ftIhB1uhevdso1_400.jpg", "500");
   addCat("Max", "Whiskas Temptations", "http://66.media.tumblr.com/7c5784ea89369c780e782bf10c60315a/tumblr_npb0hlYwhV1u63jaco1_1280.jpg", "500")
-  addCat("Cat in Tube", "Cardboard", "https://i.ytimg.com/vi/tntOCGkgt98/maxresdefault.jpg", "500");
+  addCat("Cat in Tube", "Cardboard", "https://i.ytimg.com/vi/tntOCGkgt98/maxresdefault.jpg");
+  addCat("Cat in the Hat", "Green Eggs and Ham", "http://vignette3.wikia.nocookie.net/seuss/images/7/76/Dr-seuss.jpg/revision/latest?cb=20121016165122");
 }
 
 
